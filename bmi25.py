f414c2c17822e4f03ea44fadc58c752be3ec6631
intetibunny-ux <intetibunny@gmail.com>
2026-07-25 21:09:03 +0530

    Add files via upload

---
+++ bmi25.py
@@ -0,0 +1,30 @@
+#BML Calculator
+while True:
+'''a=int(input("enter the weight of the person:"))
+b=int(input("enter the weight of the person:"))
+BML Calcutor =weight/(height)**2
+if BMI 18.5 > 0:
+    print("under weight")
+elif BMI is >18.5 and 24.5:
+    print("healthy weight")
+ elif BMI is >24.5 and 29.5:
+     print("over weight")
+ elif BMI then 30:
+     print("obsesity")
+
+     
+#patterns
+#1 right angle trianglr
+'''a=int(input())
+for i in range(1,a+1):
+    print("*" *1)
+
+
+
+#2 reverse right angle triangle
+'''a=int(input())
+for i in range(a,=0,-1):
+print("*"*i)'''
+
+
+
